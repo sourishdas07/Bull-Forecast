c701110a6aaf1dbe8cb68015c3396456c13729e4
Sourish Das <109391867+sourishdas07@users.noreply.github.com>
2022-12-20 02:40:23 -0500

    Add files via upload
    The Fundamental Understanding Behind How Predictions are Made.

--- main.py
+++ main.py
@@ -1,75 +1,55 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
-import yfinance as yf
 import pandas as pd
-from datetime import date
-from datetime import timedelta, datetime
+import pandas_datareader
+import datetime as dt
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import time
+from textblob import TextBlob
 
 
-# Styling
-hide_st_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                </style>
-                """
+ticker = input("Enter a ticker: ")
 
-st.markdown(hide_st_style, unsafe_allow_html=True)
+start_date = dt.datetime(2020, 1, 1)
+end_date = dt.datetime(2022, 7, 30)
+stock_info = pandas_datareader.DataReader(
+    ticker, 'yahoo', start_date, end_date)
 
+data = stock_info[['Close']]
+data = data.rename(columns={'Close': 'True_Close'})
+data['Target'] = stock_info.rolling(2).apply(
+    lambda x: x.iloc[1] > x.iloc[0])['Close']
 
-# Sidebar
-with st.sidebar:
-    selected = option_menu(
-        menu_title= "",
-        options = ["performance overview", "relative returns"],
-    )
-
-
-if selected == "performance overview":
-
-    st.title("Performance Overview")
-
-    start_date = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
-    end_date = st.date_input('End', value = pd.to_datetime('today'))
-
-    indicators = ('Relative Return', 'Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume')
-    selected_indicator = st.multiselect("Select An Indicator", indicators)
-
-    df = pd.read_excel('company_list.xlsx')
-    tickers = df['Symbol'].values.tolist()
-    dropdown = st.multiselect("Select A Stock", tickers)
-
-    if len(dropdown) > 0:
-        df = yf.download(dropdown, start_date, end_date)[selected_indicator]
-        st.line_chart(df)
-
-        
-if selected == "relative returns":
-
-    st.title("Relative Returns")
-
-    start_date = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
-    end_date = st.date_input('End', value = pd.to_datetime('today'))
-
-    df = pd.read_excel('company_list.xlsx')
-    tickers = df['Symbol'].values.tolist()
-    dropdown = st.multiselect("Select A Stock", tickers)
-
-    # Relative Return
-    def relativeret(df):
-        rel = df.pct_change()
-        cumret = (1 + rel).cumprod() - 1
-        cumret = cumret.fillna(0)
-        return cumret
-
-    if len(dropdown) > 0:
-        df = relativeret(yf.download(dropdown, start_date, end_date)['Adj Close'])
-        st.line_chart(df)
-
-        
+predictors = ['Close', 'High', 'Low', 'Open', 'Volume']
+stock_prev = stock_info.copy()
+stock_prev = stock_prev.shift(1)
+cut_off = 1
+data = data.join(stock_prev[predictors]).iloc[cut_off:]
 
 
+# Part 3
+estimators = 500  # number of decision trees
+samples_split = 3  # minimum number of samples required before split
+model = RandomForestClassifier(
+    n_estimators=estimators, min_samples_split=samples_split, random_state=1)
 
-    
 
-    
+# Part 4
+pred_days = 200
+train = data.iloc[:-pred_days]
+test = data.iloc[-pred_days:]
+
+model.fit(train[predictors], train['Target'])
+
+# Part 5
+preds = (model.predict_proba(test[predictors]))[:, 1]
+preds = pd.Series(preds, index=test.index)
+target_precision = 0.8
+preds[preds >= target_precision] = 1
+preds[preds < target_precision] = 0
+
+
+# Part 6
+combined = pd.concat({'Target': test['Target'], 'Predictions': preds}, axis=1)
+plt.plot(combined['Predictions'])
+plt.plot(combined['Target'])
+plt.show()
